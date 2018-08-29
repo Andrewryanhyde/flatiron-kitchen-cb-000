d1dfbc7547d04c8fd103c2a11d9ce6c4c13797fa
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-29 09:46:16 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,16 +11,14 @@ class RecipesController < ApplicationController
 
   def update
     @recipe = Recipe.find_by_id(params[:id])
-
     @recipe.update(recipe_params)
     @recipe.recipe_ingredients.clear
-
     params[:recipe][:ingredients].each do |ingredient_id|
+      
       if ingredient_id.present?
         @recipe.recipe_ingredients.build(:ingredient_id => ingredient_id)
 
       end
-
     end
     @recipe.save
     redirect_to recipe_path(@recipe)
@@ -30,21 +28,16 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.create(recipe_params)
-
     params[:recipe][:ingredients].each do |ingredient_id|
       if ingredient_id.present?
         @recipe.recipe_ingredients.build(:ingredient_id => ingredient_id)
         @recipe.save
       end
-
     end
-
-
     redirect_to recipe_path(@recipe)
   end
 
   def show
-
     @recipe = Recipe.find_by_id(params[:id])
   end
 
