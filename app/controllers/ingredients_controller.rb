60d910a5ed7bdd1f2416f47d04a2a3ef6d50109a
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-29 09:45:09 +0000

    automatically backed up by learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,23 +1,28 @@
 class IngredientsController < ApplicationController
-
   def new
     @ingredient = Ingredient.new
   end
 
   def update
+
     @ingredient = Ingredient.find_by_id(params[:id])
+
     @ingredient.update(ingredient_params)
     @ingredient.save
     redirect_to ingredient_path(@ingredient)
   end
 
+
+
   def create
+    
     @ingredient = Ingredient.create(ingredient_params)
 
     redirect_to @ingredient
   end
 
   def show
+
     @ingredient = Ingredient.find_by_id(params[:id])
   end
 
@@ -30,5 +35,4 @@ class IngredientsController < ApplicationController
   def ingredient_params
     params.require(:ingredient).permit(:name)
   end
-  
 end
