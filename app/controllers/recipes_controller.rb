9d930cfbbfd8960c52fb7ec1ef9bde13c8555d7c
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-29 09:39:23 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -49,7 +49,7 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name)
+    params.require(:recipe).permit(:name, :ingredients)
   end
   
 end
