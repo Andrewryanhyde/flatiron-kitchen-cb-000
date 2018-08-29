eb758fd53fd57e09b8a55445e78f69a60c906f90
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-29 09:37:13 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,18 @@
 class RecipesController < ApplicationController
+
+
+  def new 
+    @recipe = Recipe.new 
+  end 
+
+  def edit 
+    @recipe = Recipe.find_by_id(params[:id])
+  end 
+
+  def update 
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.update(recipe_params)
+    @recipe.save 
+    redirect_to recipe_path(@recipe)
+  end
 end
